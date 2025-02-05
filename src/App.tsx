4f0b16bc1ee49937c1b07c6135ee96662120b440
Wei Ouyang <oeway007@gmail.com>
2025-02-05 13:28:08 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Bioimage Model Zoo</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
