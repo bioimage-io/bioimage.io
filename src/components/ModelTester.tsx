3868e1c0021a52603d87d3fb35a97644061fa82c
Wei Ouyang <oeway007@gmail.com>
2025-05-26 01:02:09 +0200

    switch model runner

--- src/components/ModelTester.tsx
+++ src/components/ModelTester.tsx
@@ -98,12 +98,17 @@ const ModelTester: React.FC<ModelTesterProps> = ({ artifactId, modelUrl, isDisab
     
     try {
       setLoadingStep('Connecting to model runner service...');
-      const runner = await server.getService('bioimage-io/bioimageio-model-runner', {mode: "last"});
+      const bioEngineApps = await server.getService('bioimage-io/bioengine-apps', {mode: "last"});
+      const runner = bioEngineApps.bioimage_io_model_runner;
+      if(!runner){
+        throw Error("Model Runner is not available")
+      }
+
       const modelId = artifactId.split('/').pop();
       
       setLoadingStep('Downloading and preparing model for testing...');
       console.log(`Testing model ${modelId} at ${modelUrl}`);
-      const result = await runner.test(modelId, modelUrl);
+      const result = await runner.test({model_id: modelUrl || modelId, _rkwargs: true});
       console.log("Test result:", result);
       setTestResult(result);
     } catch (err) {
